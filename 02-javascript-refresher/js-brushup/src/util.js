60bf07b084105da748cdbc1917d9cc4a89b248d7
Apurb Choudhary <apurbkumar.choudhary@gmail.com>
2023-08-15 03:00:28 +0530

    section 2 complete

---
+++ 02-javascript-refresher/js-brushup/src/util.js
@@ -0,0 +1,6 @@
+export let apiKey1 = 'dsfsrtwetdsxs42323';
+export let apiKey2 = 'dsfsrtwetdsxs42323';
+export let apiKey3 = 'dsfsrtwetdsxs42323';
+export let apiKey4 = 'dsfsrtwetdsxs42323';
+export let apiKey5 = 'dsfsrtwetdsxs42323';
+export default 'ert4535dvvxc';
